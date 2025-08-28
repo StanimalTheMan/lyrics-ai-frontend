b82d82ab8ceaa30fc8040b9b8b10e7cf9d10a1f4
Stan Choi <sjc591@nyu.edu>
2025-08-27 22:39:23 -0400

    Fix import

--- app/oauth2/callback/page.tsx
+++ app/oauth2/callback/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect } from "react"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { useAuth } from "../../../context/AuthContext"
 
 export default function OAuthCallback() {
